1c1192d4631e1d65c4fc8ed058f27f8d9da235ca
Manaf <abdu.s.manaf@gmail.com>
2026-05-18 12:10:23 +0530

    Add AWS EC2 instance resource configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,12 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+resource "aws_instance" "myserver" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "MyServer"
+  }
+}
